51b6ddd16035323ab262165561bbe3bc81d67d64
maurya98 <35663315+maurya98@users.noreply.github.com>
2019-04-07 00:17:26 +0530

    Add files via upload

--- src/Controller/MyController.java
+++ src/Controller/MyController.java
@@ -1,1 +1,42 @@
+package controller;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import model.*;
+import java.io.*;
+import dao.UserDao;
+public class MyController extends HttpServlet {
 
+	public void srvice(HttpServletRequest req,HttpServletResponse res) throws ServletException,IOException
+	{
+		res.setContentType("text/html");
+		PrintWriter out=res.getWriter();
+		String UserName=req.getParameter("Username");
+		String UserPassword=req.getParameter("UserPassword");
+		User user=new User();
+		user.setUserName(UserName);
+		user.setUserPassword(UserPassword);
+		
+		try {
+			UserDao userDao=new UserDao();
+			Boolean b=userDao.validate(user);
+			if(b==true)
+			{
+				RequestDispatcher rd=req.getRequestDispatcher("wel");
+				rd.forward(req,res);
+			//res.sendRedirect("wel");
+			}
+	
+			else
+			{
+				RequestDispatcher rd=req.getRequestDispatcher("err");
+				rd.include(req,res);
+				
+				//res.sendRedirect("err");
+			}
+		
+		}
+		catch(Exception e) {
+			out.println(e);
+		}
+			}
+}
