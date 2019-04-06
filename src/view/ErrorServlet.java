daf64d10f614d75fd0ac3d692c3b7c6861daa514
maurya98 <35663315+maurya98@users.noreply.github.com>
2019-04-07 00:22:27 +0530

    Add files via upload

--- src/view/ErrorServlet.java
+++ src/view/ErrorServlet.java
@@ -3,11 +3,11 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
 
-public class WelcomeServlet extends HttpServlet{
+public class ErrorServlet extends HttpServlet{
 public void service(HttpServletRequest req,HttpServletResponse res) throws ServletException,IOException
 {
 	res.setContentType("text/html");
 	PrintWriter out=res.getWriter();
-	out.println("<h1>Welcome</h1>");
+	out.println("<h1>Check your id and password</h1>");
 }
 }
